75acd2882896b53bdb7feb5539fb3c23882c9c5a
Szymzal <szymzal05@gmail.com>
2021-10-26 17:51:35 +0200

    Fixed the annoying light bulb

--- nvim/.config/nvim/lua/_lspsaga.lua
+++ nvim/.config/nvim/lua/_lspsaga.lua
@@ -3,7 +3,14 @@ require('lspsaga').init_lsp_saga {
 	border_style = "round",
 	error_sign = '',
 	warn_sign = '',
-	hint_sign = ' ',
+    hint_sign = '',
 	infor_sign = '',
+    -- This diables the annoying light bulb
+    code_action_prompt = {
+        enable = false,
+        sign = false,
+        sign_priority = 0,
+        virtual_text = false,
+    },
 	max_preview_lines = 25,
 }
