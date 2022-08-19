216f9a28a8a0b1c144858eb8d7667c12c0aba09d
Szymzal <szymzal05@gmail.com>
2022-08-19 20:07:10 +0200

    Added useful treesitter hightlighting to languages I didn't installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "c", "cpp", "rust" },
+    ensure_installed = { "c", "cpp", "rust", "html", "css", "json", "toml", "vue", "javascript", "typescript", "bash", "markdown", "json5", "lua" },
     sync_install = false,
     highlight = {
         enable = true,
