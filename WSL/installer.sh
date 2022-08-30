98e5a38b70a6a523bd404ef6921b2c029159cdad
Szymzal <szymzal05@gmail.com>
2022-08-30 13:17:22 +0200

    Changing

--- WSL/installer.sh
+++ WSL/installer.sh
@@ -2,14 +2,11 @@
 
 function packages() {
     # Installing essential packages
-    pikaur -S --noconfirm rustup stow alacritty fish tmux \
-        python3 python-pip xclip neovim \
-        zerotier-one bottles jdk17-openjdk jdk8-openjdk polymc-bin obs-studio wireshark-qt gimp zoom lxsession-gtk3 \
-        steam tree \
-        rofi i3-gaps polybar nitrogen picom \
-        webkit2gtk curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips \
-        nodejs npm vscode-langservers-extracted lua-language-server fzf
-	    #archlinux-keyring qemu virt-manager virt-viewer dnsmasq bridge-utils libguestfs \
+    pikaur -S --noconfirm rustup stow zsh tmux \
+        python3 python-pip neovim \
+        tree \
+        curl wget openssl \
+        nvm vscode-langservers-extracted lua-language-server fzf rust-analyzer
 
     sudo npm install -g typescript-language-server typescript @volar/vue-language-server bash-language-server
 
@@ -30,59 +27,22 @@ function aurhelper() {
 
 function config_files() {
     setup_config "scripts"
-    setup_config "profile"
-    setup_config "alacritty"
-    setup_config "fish"
+    setup_config "zsh"
     setup_config "tmux"
     setup_config "nvim"
-    setup_config "rofi"
-    setup_config "polybar"
-    setup_config "i3"
 }
 
 function addition_apps() {
     $HOME/.scripts/update
 }
 
-function fonts() {
-    # FiraCode
-    mkdir $HOME/firacode
-    pushd $HOME/firacode
-    curl -s https://api.github.com/repos/tonsky/FiraCode/releases/latest \
-    | grep "Fira_Code_v.*zip" \
-    | cut -d : -f 2,3 \
-    | tr -d \" \
-    | wget -qi -
-    unzip *.zip -d extracted
-    popd
-    pushd $HOME/firacode/extracted/ttf
-    sudo mv *.ttf /usr/share/fonts/TTF/
-    popd
-    rm -rf $HOME/firacode
-
-    # Material icons
-    mkdir $HOME/material-icons
-    pushd $HOME/material-icons
-    wget https://github.com/google/material-design-icons/raw/master/font/MaterialIcons-Regular.ttf
-    sudo mv MaterialIcons-Regular.ttf /usr/share/fonts/TTF/
-    popd
-    rm -rf $HOME/material-icons
-
-    fc-cache -f -v
-}
-
 function shell() {
-    chsh -s /bin/fish
+    chsh -s /bin/zsh
 }
 
 function neovim() {
     python3 -m pip install --user --upgrade pynvim
     nvim --headless -c 'autocmd User PackerComplete quitall'
-}
-
-function services() {
-    sudo systemctl enable zerotier-one.service
-    sudo systemctl start zerotier-one.service
 }
 
 function setup_config() {
@@ -128,8 +88,6 @@ function main() {
     config_files
     addition_apps
     shell
-    fonts
-    services
     neovim
 
     echo "Installator finished! You would need to restart computer to work everything properly."
