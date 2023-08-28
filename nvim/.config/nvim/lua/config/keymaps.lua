93b41279bb26ca0e8ab738b910ff8046d4eb1bc3
Szymzal <szymzal05@gmail.com>
2023-08-28 21:50:22 +0200

    Update

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+keymap("n", "<leader>vim", function()
+  require("telescope.builtin").find_files({
+    prompt_title = "<vimrc>",
+    cwd = vim.fn.stdpath("config"),
+  })
+end, opts)
