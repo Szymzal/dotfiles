10b3539c66f837c7fcc1f9f28524b4301899e687
Szymzal <szymzal05@gmail.com>
2024-08-08 10:39:58 +0200

    Update

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ If you really want you can use them, but I don't recommend.
 
 ## Remember to check
 - [ ] [Nouveau weird cursor](https://github.com/hyprwm/Hyprland/issues/5776)
-- [ ] [NVIDIA 555 driver becomes stable](https://www.nvidia.com/download/driverResults.aspx/224350/en-us/)
-- [ ] [Hyprcursor not working on NixOS](https://github.com/hyprwm/hyprcursor/issues/19)
--- nix/nixosModules/nvidia.nix
+++ nix/nixosModules/nvidia.nix
@@ -41,15 +41,7 @@ in
         powerManagement.finegrained = false;
         open = false;
         nvidiaSettings = true;
-        # Driver 555 fixes flickering on xwayland
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "555.42.02";
-          sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-          sha256_aarch64 = fakeSha256;
-          openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-          settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-          persistencedSha256 = fakeSha256;
-        };
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
       };
 
       # Sometimes after update "ghost" monitor can appear
