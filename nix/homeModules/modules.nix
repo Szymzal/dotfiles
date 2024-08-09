4551cd68ae9fab7a733179919b12c2d2d261e021
Szymzal <szymzal05@gmail.com>
2024-08-09 22:20:59 +0200

    How to make it faster

--- nix/homeModules/modules.nix
+++ nix/homeModules/modules.nix
@@ -1,40 +1,42 @@
 { ... }: {
   imports = [
-    ./ags
-    ./impermanence.nix
-    ./git.nix
-    ./launcher.nix
-    ./statusBar.nix
-    ./terminal.nix
-    ./tmux.nix
-    ./wm.nix
-    ./coding
-    ./cd.nix
-    ./ls.nix
-    ./find.nix
-    ./notifications.nix
-    ./file-explorer.nix
-    ./communication.nix
-    ./browser.nix
-    ./image-editor.nix
-    ./viewers.nix
-    ./screenshot.nix
-    ./notes.nix
-    ./theme.nix
-    ./calendar.nix
-    ./casparcg.nix
-    ./bottles.nix
-    ./network.nix
-    ./office.nix
-    ./video-rec.nix
-    ./shell.nix
-    ./networkmanager.nix
-    ./videoediting.nix
-    ./ldtk.nix
+    ../../modules/home/ags
 
-    ./games/minecraft.nix
-    ./games/terraria.nix
-    ./games/rocket-league
-    ./games/lutris.nix
+    # ../../modules/home-slim/home.nix
+    ../../modules/home/impermanence.nix
+    ../../modules/home/git.nix
+    ../../modules/home/launcher.nix
+    ../../modules/home/statusBar.nix
+    ../../modules/home/terminal.nix
+    ../../modules/home/tmux.nix
+    ../../modules/home/wm.nix
+    ../../modules/home/coding
+    ../../modules/home/cd.nix
+    ../../modules/home/ls.nix
+    ../../modules/home/find.nix
+    ../../modules/home/notifications.nix
+    ../../modules/home/file-explorer.nix
+    ../../modules/home/communication.nix
+    ../../modules/home/browser.nix
+    ../../modules/home/image-editor.nix
+    ../../modules/home/viewers.nix
+    ../../modules/home/screenshot.nix
+    ../../modules/home/notes.nix
+    ../../modules/home/theme.nix
+    ../../modules/home/calendar.nix
+    ../../modules/home/casparcg.nix
+    ../../modules/home/bottles.nix
+    ../../modules/home/network.nix
+    ../../modules/home/office.nix
+    ../../modules/home/video-rec.nix
+    ../../modules/home/shell.nix
+    ../../modules/home/networkmanager.nix
+    ../../modules/home/videoediting.nix
+    ../../modules/home/ldtk.nix
+
+    ../../modules/home/games/minecraft.nix
+    ../../modules/home/games/terraria.nix
+    ../../modules/home/games/rocket-league
+    ../../modules/home/games/lutris.nix
   ];
 }
