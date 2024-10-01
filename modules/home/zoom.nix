78d35b3c3fc9a33d7f5ef4b912a438f0556d15e6
Szymzal <szymzal05@gmail.com>
2024-10-01 23:56:24 +0200

    Trying to fix zoom

--- modules/home/zoom.nix
+++ modules/home/zoom.nix
@@ -12,7 +12,14 @@ in
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      zoom-us
+      (zoom-us.overrideAttrs (attrs: {
+        nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+        postFixup = (attrs.postFixup or "") + ''
+          wrapProgram $out/opt/zoom/zoom \
+            --set XDG_SESSION_TYPE "" \
+            --set WAYLAND_DISPLAY ""
+        '';
+      }))
     ];
 
     mypackages.impermanence.files = [
--- modules/nixos/zoom.nix
+++ modules/nixos/zoom.nix
@@ -1,10 +1,16 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
 let
   myLib = config.lib.myLib;
 in
 {
-  mypackages.unfree.allowed = mkIf ((myLib.isEnabledOptionOnHomeConfig "mypackages.zoom.enable") && config.home-manager.useGlobalPkgs) [
-    "zoom"
-  ];
+  config = mkIf (myLib.isEnabledOptionOnHomeConfig "mypackages.zoom.enable") {
+    mypackages.unfree.allowed = mkIf (config.home-manager.useGlobalPkgs) [
+      "zoom"
+    ];
+
+    systemd.tmpfiles.rules = [
+      "L+ /usr/libexec/xdg-desktop-portal - - - - ${pkgs.xdg-desktop-portal}/libexec/xdg-desktop-portal"
+    ];
+  };
 }
