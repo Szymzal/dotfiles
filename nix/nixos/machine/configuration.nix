9e2b6a52d4410fdbb0bcc5a0008b633fa758c20d
Szymzal <szymzal05@gmail.com>
2024-10-16 22:30:05 +0200

    Fix minecraft server

--- nix/nixos/machine/configuration.nix
+++ nix/nixos/machine/configuration.nix
@@ -231,7 +231,7 @@ in
               jvmOpts = "-Xmx8G -Xms8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
               package = pkgs.fabricServers.fabric-1_20_1;
               serverProperties = {
-                server-port = 25565;
+                server-port = 25576;
                 allow-flight = true;
                 broadcast-console-to-ops = false;
                 broadcast-rcon-to-ops = false;
@@ -287,9 +287,11 @@ in
               autoStart = false;
               openFirewall = true;
               jvmOpts = "-Xmx8G -Xms8G";
-              package = pkgs.fabricServers.fabric-1_20_1;
+              package = pkgs.fabricServers.fabric-1_20_1.override {
+                loaderVersion = "0.16.5";
+              };
               serverProperties = {
-                server-port = 25571;
+                server-port = 25565;
                 allow-flight = true;
                 broadcast-console-to-ops = false;
                 broadcast-rcon-to-ops = false;
