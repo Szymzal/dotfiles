961b2de8a87b69b11172f07aaf105fe4d8304276
Szymzal <szymzal05@gmail.com>
2024-11-11 23:47:36 +0100

    Update

--- modules/home/blender.nix
+++ modules/home/blender.nix
@@ -15,25 +15,7 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      # TODO: remove when merged to unstable
-      # https://nixpk.gs/pr-tracker.html?pr=354095
-      (blender.override {
-        openvdb = openvdb.overrideAttrs (attrs: rec {
-          name = "${attrs.pname}-${version}";
-          version = "11.0.0";
-          src = fetchFromGitHub {
-            owner = "AcademySoftwareFoundation";
-            repo = "openvdb";
-            rev = "v${version}";
-            sha256 = "sha256-wDDjX0nKZ4/DIbEX33PoxR43dJDj2NF3fm+Egug62GQ=";
-          };
-          meta =
-            attrs.meta
-            // {
-              license = lib.licenses.mpl20;
-            };
-        });
-      })
+      blender
     ];
 
     mypackages.impermanence.directories = [
--- nix/nixos/machine/configuration.nix
+++ nix/nixos/machine/configuration.nix
@@ -125,6 +125,7 @@ in {
 
   environment.systemPackages = with pkgs; [
     btop
+    zotero
   ];
 
   mypackages = {
