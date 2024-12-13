8d1fb464fb44b01446ffde0975c1331dd4032ea9
Szymzal <szymzal05@gmail.com>
2024-12-13 14:30:20 +0100

    Well, it looks ok, but games are not ok with it

--- nix/nixos/machine/configuration.nix
+++ nix/nixos/machine/configuration.nix
@@ -147,7 +147,7 @@ in {
     git.enable = true;
     multiTerminal.enable = true;
     cachix.enable = true;
-    cuda.enable = false;
+    cuda.enable = true;
     virtualization.enable = true;
 
     monitors = {
@@ -232,7 +232,7 @@ in {
     video-recording.enable = true;
     nvidia = {
       enable = true;
-      open.enable = true;
+      open.enable = false;
     };
     cd.enable = true;
     ls.enable = true;
