568cd57ee0bf2cf938428fc719bc14f1a5e04db9
Szymzal <szymzal05@gmail.com>
2025-01-03 16:52:12 +0100

    Fixing thunar

--- modules/nixos/file-explorer.nix
+++ modules/nixos/file-explorer.nix
@@ -7,16 +7,37 @@
 with lib; let
   myLib = config.lib.myLib;
 in {
-  config = mkIf (myLib.isEnabledOptionOnHomeConfig "mypackages.file-explorer.enable") {
+  config = mkIf (myLib.isEnabledOptionOnHomeConfig "mypackages.file-explorer.enable") (let
+    # NOTE: Why so many services have broken PATH variable? (thunar, foot)
+    script = pkgs.writeShellScript "thunar-server" ''
+      PATH=$PATH:/etc/profiles/per-user/$USER/bin/:$HOME/.nix-profile/bin/:/run/current-system/sw/bin/
+      $1 --daemon
+    '';
+
+    package =
+      ((pkgs.xfce.thunar.overrideAttrs (attrs: {
+        buildCommand = attrs.buildCommand + ''
+          substituteInPlace $out/lib/systemd/user/thunar.service \
+            --replace-warn "ExecStart=" "ExecStart=${script} " \
+            --replace-warn " --daemon" ""
+        '';
+      }))
+      .override {thunarPlugins = [ pkgs.xfce.thunar-archive-plugin ];});
+  in {
     programs.dconf.enable = true;
 
-    programs.thunar = {
-      enable = true;
-      plugins = with pkgs; [
-        xfce.thunar-archive-plugin
-      ];
-    };
+    environment.systemPackages = [
+      package
+    ];
+
+    services.dbus.packages = [
+      package
+    ];
+
+    systemd.packages = [
+      package
+    ];
 
     services.gvfs.enable = true;
-  };
+  });
 }
