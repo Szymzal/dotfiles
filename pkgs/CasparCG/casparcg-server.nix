d40656532483a55bade6d537dc190ff6383176ff
Szymzal <szymzal05@gmail.com>
2025-01-10 21:36:41 +0100

    It compiles

--- pkgs/CasparCG/casparcg-server.nix
+++ pkgs/CasparCG/casparcg-server.nix
@@ -129,7 +129,7 @@ in {
     ]
     ++ lib.optionals enable_html [
       "-DCEF_LIB_PATH=${libcef}/lib"
-      "-DCEF_INCLUDE_PATH=${libcef}/include"
+      "-DCEF_INCLUDE_PATH=${libcef}"
       "-DENABLE_HTML=ON" # TODO: until I find a way to install caspar-cef-117: https://github.com/CasparCG/server/blob/master/src/CMakeModules/Bootstrap_Linux.cmake#L62
       "-DUSE_SYSTEM_CEF=ON"
     ];
