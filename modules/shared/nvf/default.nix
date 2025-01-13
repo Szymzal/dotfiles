21e18e2344aeedc6863f1b36b667aeb1d767344b
Szymzal <szymzal05@gmail.com>
2025-01-13 14:49:15 +0100

    Default configuration

--- modules/shared/nvf/default.nix
+++ modules/shared/nvf/default.nix
@@ -1,2 +1,151 @@
 {
+  config.vim = {
+    viAlias = true;
+    vimAlias = true;
+
+    spellcheck = {
+      enable = true;
+    };
+
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+      lightbulb.enable = true;
+      trouble.enable = true;
+      lspSignature.enable = true;
+      otter-nvim.enable = true;
+      lsplines.enable = true;
+    };
+
+    languages = {
+      enableLSP = true;
+      enableFormat = true;
+      enableTreesitter = true;
+      enableExtraDiagnostics = true;
+
+      nix.enable = true;
+      markdown.enable = true;
+
+      bash.enable = true;
+      css.enable = true;
+      html.enable = true;
+      ts.enable = true;
+      go.enable = true;
+      lua.enable = true;
+      python.enable = true;
+      rust = {
+        enable = true;
+        crates.enable = true;
+      };
+    };
+
+    visuals = {
+      nvim-web-devicons.enable = true;
+      nvim-cursorline.enable = true;
+      cinnamon-nvim.enable = true;
+      fidget-nvim.enable = true;
+
+      highlight-undo.enable = true;
+      indent-blankline.enable = true;
+    };
+
+    statusline = {
+      lualine = {
+        enable = true;
+        theme = "catppuccin";
+      };
+    };
+
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
+      transparent = false;
+    };
+
+    autopairs.nvim-autopairs.enable = true;
+
+    autocomplete.nvim-cmp.enable = true;
+    snippets.luasnip.enable = true;
+
+    filetree = {
+      neo-tree = {
+        enable = true;
+      };
+    };
+
+    tabline = {
+      nvimBufferline.enable = true;
+    };
+
+    treesitter.context.enable = true;
+
+    binds = {
+      whichKey.enable = true;
+      cheatsheet.enable = true;
+    };
+
+    telescope.enable = true;
+
+    git = {
+      enable = true;
+      gitsigns.enable = true;
+      gitsigns.codeActions.enable = false;
+    };
+
+    minimap = {
+      codewindow.enable = true;
+    };
+
+    dashboard = {
+      alpha.enable = true;
+    };
+
+    notify = {
+      nvim-notify.enable = true;
+    };
+
+    projects = {
+      project-nvim.enable = true;
+    };
+
+    utility = {
+      surround.enable = true;
+      motion = {
+        hop.enable = true;
+        leap.enable = true;
+        precognition.enable = true;
+      };
+    };
+
+    notes = {
+      todo-comments.enable = true;
+    };
+
+    terminal = {
+      toggleterm = {
+        enable = true;
+        lazygit.enable = true;
+      };
+    };
+
+    ui = {
+      borders.enable = true;
+      noice.enable = true;
+      colorizer.enable = true;
+      illuminate.enable = true;
+      breadcrumbs = {
+        enable = true;
+        navbuddy.enable = true;
+      };
+      smartcolumn = {
+        enable = true;
+      };
+      fastaction.enable = true;
+    };
+
+    comments = {
+      comment-nvim.enable = true;
+    };
+  };
 }
