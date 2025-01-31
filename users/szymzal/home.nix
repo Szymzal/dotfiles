1feff69cbc917caeb0772647135a797c3d429322
Szymzal <szymzal05@gmail.com>
2025-01-31 13:42:02 +0100

    Update without GIMP :(

--- users/szymzal/home.nix
+++ users/szymzal/home.nix
@@ -60,7 +60,7 @@ in {
     communication.enable = true;
     browser.enable = true;
     viewers.enableAll = true;
-    image-editors.enable = true;
+    image-editors.enable = false; # TODO: Revert
     screenshot = {
       enable = true;
       savePicturesPath = "Pictures/Screenshots";
