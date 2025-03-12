7a6309df47037fc17aa0e5b09155925dfeba786a
Szymzal <szymzal05@gmail.com>
2025-03-12 15:05:01 +0100

    Update Casparcg-server

--- pkgs/CasparCG/casparcg-server.nix
+++ pkgs/CasparCG/casparcg-server.nix
@@ -37,8 +37,47 @@
   enable_html ? true,
 }:
 stdenv.mkDerivation (let
-  version = "2.4.2";
+  version = "2.4.3";
   release = "stable";
+
+  freeimage-patched = freeimage.overrideAttrs (_: {
+    src = fetchsvn {
+      url = "svn://svn.code.sf.net/p/freeimage/svn/";
+      rev = "1909";
+      hash = "sha256-xgOFjonh/1oKUM6je8fMeswYp8Z9kwva5Cm05Wqu9AY=";
+    };
+
+    patches = [
+      # Updated Original Patch
+      ./unbundle.diff
+
+      # Patches from Fedora
+      # CVE-2020-29292
+      (fetchpatch2 {
+        url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24292.patch";
+        hash = "sha256-U2TjHrV0PQrqnMic0+VWF3mBdZvu6CN+/+vpeNolGXU=";
+      })
+      # CVE-2020-29293
+      (fetchpatch2 {
+        url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24293.patch";
+        hash = "sha256-kmY584C17Xi4+qqJAnN0M/3OB4K4Sa4M3jdJwvx32ig=";
+      })
+      # CVE-2020-29295
+      (fetchpatch2 {
+        url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24295.patch";
+        hash = "sha256-B4Ls9lyizdgkEqyYDkIpqOBcWkekbYRhuadQJchtt30=";
+      })
+      ./CVE-2021-33367.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-33367.patch
+      ./CVE-2021-40263.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-40263.patch
+      ./CVE-2021-40266.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-40266.patch
+      # CVE-2023-47995
+      (fetchpatch2 {
+        url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2023-47995.patch";
+        hash = "sha256-aET+Is8Mr9E+NV0VOQg1auodyuAAyrU8uM7wLl4HHSc=";
+      })
+      ./CVE-2023-47997.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2023-47997.patch
+    ];
+  });
 
   cef = stdenvNoCC.mkDerivation (let
     LIB = "${libcef}/lib";
@@ -56,6 +95,8 @@ stdenv.mkDerivation (let
       ln -s ${LIB}/* ${OUT}/
     '';
   });
+
+  cef_out = "${cef}/lib/casparcg-cef";
 in {
   pname = "CasparCG-server";
   inherit version;
@@ -63,7 +104,7 @@ in {
     owner = "CasparCG";
     repo = "server";
     rev = "refs/tags/v${version}-${release}";
-    hash = "sha256-AnI7MmCwG2El/+K2RDYCaF690LnjNPlT5RahT7CKWNo=";
+    hash = "sha256-w0jBb4xdYyzNR7pYo9fg07jJ78k8DhROGog+VlohG3Y=";
   };
 
   nativeBuildInputs = [
@@ -87,44 +128,7 @@ in {
     expat # libexpat1-dev
     lsb-release
     glew # libglew-dev
-    (freeimage.overrideAttrs {
-      src = fetchsvn {
-        url = "svn://svn.code.sf.net/p/freeimage/svn/";
-        rev = "1909";
-        hash = "sha256-xgOFjonh/1oKUM6je8fMeswYp8Z9kwva5Cm05Wqu9AY=";
-      };
-
-      patches = [
-        # Updated Original Patch
-        ./unbundle.diff
-
-        # Patches from Fedora
-        # CVE-2020-29292
-        (fetchpatch2 {
-          url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24292.patch";
-          hash = "sha256-U2TjHrV0PQrqnMic0+VWF3mBdZvu6CN+/+vpeNolGXU=";
-        })
-        # CVE-2020-29293
-        (fetchpatch2 {
-          url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24293.patch";
-          hash = "sha256-kmY584C17Xi4+qqJAnN0M/3OB4K4Sa4M3jdJwvx32ig=";
-        })
-        # CVE-2020-29295
-        (fetchpatch2 {
-          url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2020-24295.patch";
-          hash = "sha256-B4Ls9lyizdgkEqyYDkIpqOBcWkekbYRhuadQJchtt30=";
-        })
-        ./CVE-2021-33367.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-33367.patch
-        ./CVE-2021-40263.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-40263.patch
-        ./CVE-2021-40266.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2021-40266.patch
-        # CVE-2023-47995
-        (fetchpatch2 {
-          url = "https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2023-47995.patch";
-          hash = "sha256-aET+Is8Mr9E+NV0VOQg1auodyuAAyrU8uM7wLl4HHSc=";
-        })
-        ./CVE-2023-47997.patch # Modified https://src.fedoraproject.org/rpms/freeimage/raw/rawhide/f/CVE-2023-47997.patch
-      ];
-    }) # libfreeimage-dev
+    freeimage-patched # libfreeimage-dev
     tbb # libtbb-dev
     openal # libopenal-dev
     xorg.libXcursor # libxcursor-dev
@@ -140,33 +144,35 @@ in {
     icu
   ];
 
+  preBuild = lib.optionals enable_html ''
+    BOOTSTRAP_PATH=/build/source/src/CMakeModules/Bootstrap_Linux.cmake
+    CEF_LIB_REPLACE=$(cat $BOOTSTRAP_PATH | grep "set(CEF_LIB_PATH \"/" | cut -d "\"" -f2)
+    CEF_INCLUDE_REPLACE=$(cat $BOOTSTRAP_PATH | grep "set(CEF_INCLUDE_PATH \"/" | cut -d "\"" -f2)
+
+    substituteInPlace $BOOTSTRAP_PATH \
+      --replace-fail $CEF_LIB_REPLACE ${cef_out} \
+      --replace-fail $CEF_INCLUDE_REPLACE ${libcef}
+  ''; # Fix to be able to use custom CEF binary
+
   cmakeFlags =
     [
       "-DUSE_SYSTEM_FFMPEG=ON"
       "-DUSE_STATIC_BOOST=OFF"
     ]
-    ++ lib.optionals enable_html (let
-      OUT = "${cef}/lib/casparcg-cef";
-    in [
-      "-DCEF_LIB_PATH=${OUT}"
-      "-DCEF_INCLUDE_PATH=${libcef}"
-      "-DCEF_BIN_PATH=${OUT}"
-      "-DENABLE_HTML=ON" # TODO: until I find a way to install caspar-cef-117: https://github.com/CasparCG/server/blob/master/src/CMakeModules/Bootstrap_Linux.cmake#L62
-      "-DUSE_SYSTEM_CEF=ON"
-    ]);
-
-  patches =
-    [
-      # FIX: Wait to it to be deployed: https://github.com/CasparCG/server/pull/1584
-      (fetchpatch2 {
-        url = "https://patch-diff.githubusercontent.com/raw/CasparCG/server/pull/1584.patch";
-        hash = "sha256-8XdluwjXrCjC7YkuKlMnm7d++fslcSthGR7iLTbw22Q=";
-      })
-      ./fix_ffmpeg7.1_build.patch
-    ]
     ++ lib.optionals enable_html [
-      ./custom_cef_binary.patch
+      "-DCEF_BIN_PATH=${cef_out}"
+      "-DENABLE_HTML=ON"
+      "-DUSE_SYSTEM_CEF=ON"
     ];
+
+  patches = [
+    # FIX: Wait to it to be deployed: https://github.com/CasparCG/server/pull/1584
+    (fetchpatch2 {
+      url = "https://patch-diff.githubusercontent.com/raw/CasparCG/server/pull/1584.patch";
+      hash = "sha256-8XdluwjXrCjC7YkuKlMnm7d++fslcSthGR7iLTbw22Q=";
+    })
+    ./fix_ffmpeg7.1_build.patch # FIX: Temporary fix, we don't know what consequences it will have. See: https://github.com/CasparCG/server/issues/1586
+  ];
 
   cmakeDir = "../src";
 
