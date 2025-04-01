ea2e60077bce8470301bf3538888a5310a9f2a5d
Szymzal <szymzal05@gmail.com>
2025-04-01 15:20:38 +0200

    Update

--- modules/home/games/roblox.nix
+++ modules/home/games/roblox.nix
@@ -16,10 +16,11 @@ in {
     mypackages.flatpak = {
       enable = true;
       packages = [
-        {
-          flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
-          sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
-        }
+        "org.vinegarhq.Sober"
+        # {
+        #   flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+        #   sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+        # }
       ];
     };
   };
