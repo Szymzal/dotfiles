37734aed9158b047b878ce4cb3389a4e8ea093fa
Szymzal <szymzal05@gmail.com>
2025-09-29 14:56:30 +0200

    Remove Gromix-mtx

--- modules/home/wm.nix
+++ modules/home/wm.nix
@@ -131,18 +131,6 @@ in {
               "HYPRCURSOR_SIZE,${toString config.mypackages.theme.cursorTheme.size}"
             ];
 
-            workspace = [
-              "special:gromit, gapin:0, gapsout:0, on-created-empty: ${getExe pkgs.gromit-mpx} -a"
-            ];
-
-            windowrule = [
-              "noblur, class:(Gromit-mpx)"
-              "opacity 1 override, 1 override, class:(Gromit-mpx)"
-              "noshadow, class:(Gromit-mpx)"
-              "size 100% 100%, class:(Gromit-mpx)"
-              "center, class:(Gromit-mpx)"
-            ];
-
             "$terminal" = "foot";
             "$mod" = "SUPER";
 
@@ -174,10 +162,6 @@ in {
                 ",XF86AudioMute, exec, pamixer -t"
 
                 ("$mod, P, exec, " + optionalString cfg.uwsm "uwsm app -- " + "${getExe pkgs.pkgs-unstable.grimblast} --notify --openfile --freeze copysave area")
-                "$mod, M, togglespecialworkspace, gromit"
-                ", F8, exec, ${getExe pkgs.gromit-mpx} --clear"
-                ", F7, exec, ${getExe pkgs.gromit-mpx} --undo"
-                "SHIFT, F7, exec, ${getExe pkgs.gromit-mpx} --redo"
 
                 "$mod, Z, submap, apps"
               ]
