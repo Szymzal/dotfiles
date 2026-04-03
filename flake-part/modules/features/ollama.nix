fa9e3a9eef36f67897894cecabc0029984f33f95
Szymzal <szymzal05@gmail.com>
2026-04-04 00:00:02 +0200

    Ollama 2

--- flake-part/modules/features/ollama.nix
+++ flake-part/modules/features/ollama.nix
@@ -1,13 +1,23 @@
-{
-  flake.nixosModules.ollama = {pkgs, ...}: {
+{inputs, ...}: {
+  flake.nixosModules.ollama = {pkgs, ...}: let
+    pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in {
     services = {
       ollama = {
         enable = true;
-        package = pkgs.ollama.override {
+        package = pkgs-unstable.ollama.override {
           acceleration = "vulkan";
         };
       };
       open-webui.enable = true;
     };
+    persistence.system.directories = [
+      {
+        directory = "/var/lib/private";
+        mode = "u=rwx,g=,o=";
+      }
+      "/var/lib/private/ollama"
+      "/var/lib/private/open-webui"
+    ];
   };
 }
