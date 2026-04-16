8e4d05dc8f1cceff6741608a9de132af77af8426
Szymzal <szymzal05@gmail.com>
2026-04-16 11:17:16 +0200

    Updated to latest drivers of NVIDIA

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -55,14 +55,14 @@ in {
         nvidiaPersistenced = true;
         open = true;
         # Wait until problem with missing resolution modes is fixed
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "570.195.03";
-          sha256_64bit = "sha256-1H3oHZpRNJamCtyc+nL+nhYsZfJyL7lgxPUxvXrF3B4=";
-          sha256_aarch64 = "sha256-o4rgB6vo+Cv90lJywovIyVARRGS3R15zYQUj+f1nzWQ=";
-          openSha256 = "sha256-vCBB/UJgVKHlSEWdgoF45lODr3YJmR6JwjrwWgWszBw=";
-          settingsSha256 = "sha256-mjKkMEPV6W69PO8jKAKxAS861B82CtCpwVTeNr5CqUY=";
-          persistencedSha256 = "sha256-BMpo2PIabhHjZQqUQi/W5DYhgAPmfCdFvXdN6ND2Bfs=";
-        };
+        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #   version = "570.195.03";
+        #   sha256_64bit = "sha256-1H3oHZpRNJamCtyc+nL+nhYsZfJyL7lgxPUxvXrF3B4=";
+        #   sha256_aarch64 = "sha256-o4rgB6vo+Cv90lJywovIyVARRGS3R15zYQUj+f1nzWQ=";
+        #   openSha256 = "sha256-vCBB/UJgVKHlSEWdgoF45lODr3YJmR6JwjrwWgWszBw=";
+        #   settingsSha256 = "sha256-mjKkMEPV6W69PO8jKAKxAS861B82CtCpwVTeNr5CqUY=";
+        #   persistencedSha256 = "sha256-BMpo2PIabhHjZQqUQi/W5DYhgAPmfCdFvXdN6ND2Bfs=";
+        # };
       };
 
       # Sometimes after update "ghost" monitor can appear
