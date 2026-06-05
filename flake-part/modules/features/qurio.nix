197a40e98f4fa18dcb8026ff02a36fe9b866da9d
Szymzal <szymzal05@gmail.com>
2026-06-05 19:47:13 +0200

    Fix Qurio setup

--- flake-part/modules/features/qurio.nix
+++ flake-part/modules/features/qurio.nix
@@ -42,6 +42,9 @@
       };
 
       firewall.interfaces = {
+        "${networkInterface}" = {
+          allowedUDPPorts = [67 68 53];
+        };
         "vlan-game" = {
           allowedTCPPorts = [
             80 # Captive Portal
@@ -65,41 +68,71 @@
 
       nat = {
         enable = true;
-        internalInterfaces = ["vlan-net"];
+        internalInterfaces = ["vlan-net" networkInterface];
         externalInterface = internetInterface;
       };
     };
 
-    systemd.services.dnsmasq-game = let
-      configFile = pkgs.writeText "dnsmasq-config.conf" ''
-        interface=vlan-game
-        except-interface=lo
-        bind-interfaces
-        dhcp-range=10.250.50.50,10.250.50.200,12h
-
-        dhcp-option=option:dns-server,${hostIP}
-        address=/#/${hostIP}
-      '';
-    in {
-      description = "Captive Portal DHCP/DNS for Qurio";
-      wantedBy = ["multi-user.target"];
-      after = ["network.target"];
-      serviceConfig = {
-        ExecStart = "${lib.getExe pkgs.dnsmasq} -k -C ${configFile}";
-      };
-    };
+    # systemd.services.dnsmasq-game = let
+    #   configFile = pkgs.writeText "dnsmasq-config.conf" ''
+    #     interface=vlan-game
+    #     except-interface=lo
+    #     bind-dynamic
+    #     dhcp-range=10.250.50.50,10.250.50.200,12h
+    #
+    #     dhcp-option=option:dns-server,${hostIP}
+    #     address=/#/${hostIP}
+    #   '';
+    # in {
+    #   description = "Captive Portal DHCP/DNS for Qurio";
+    #   wantedBy = ["multi-user.target"];
+    #   after = ["network.target"];
+    #   serviceConfig = {
+    #     ExecStart = "${lib.getExe pkgs.dnsmasq} -k -C ${configFile}";
+    #   };
+    # };
 
     services = {
       # Server DHCP and DNS for devices with internet access
+      # dnsmasq = {
+      #   enable = true;
+      #   resolveLocalQueries = false;
+      #   settings = {
+      #     interface = ["vlan-net" networkInterface];
+      #     except-interface = "lo";
+      #     bind-dynamic = true;
+      #
+      #     dhcp-range = [
+      #       "10.250.51.50,10.250.51.70,12h"
+      #       "10.250.49.10,10.250.49.20,12h"
+      #     ];
+      #     dhcp-option = "option:dns-server,8.8.8.8,1.1.1.1";
+      #   };
+      # };
       dnsmasq = {
         enable = true;
         resolveLocalQueries = false;
+
         settings = {
-          interface = "vlan-net";
+          interface = ["vlan-net" "vlan-game" networkInterface];
           except-interface = "lo";
-          bind-interfaces = true;
-          dhcp-range = "10.250.51.50,10.250.51.70,12h";
-          dhcp-option = "option:dns-server,8.8.8.8,1.1.1.1";
+          bind-dynamic = true;
+
+          dhcp-host = "F0:5C:19:C1:EC:52,10.250.49.10";
+
+          dhcp-range = [
+            "set:game,10.250.50.50,10.250.50.200,12h"
+            "set:net,10.250.51.50,10.250.50.70,12h"
+            "set:mgmt,10.250.49.10,10.250.49.20,12h"
+          ];
+
+          dhcp-option = [
+            "tag:game,option:dns-server,${hostIP}"
+            "tag:net,option:dns-server,8.8.8.8,1.1.1.1"
+            "tag:mgmt,option:dns-server,1.1.1.3"
+          ];
+
+          address = "/#/${hostIP}";
         };
       };
 
